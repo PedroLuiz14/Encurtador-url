ac939fa8eef877253e8a898a44d179ff84cc6098
Pedro <102750208+PedroLuiz14@users.noreply.github.com>
2022-06-26 22:45:29 -0300

    commit inicial

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { MongoConnection } from 'database/MongoConnection';
+import express from 'express';
+import { URLController } from './controller/URLController';
+
+const api = express();
+api.use(express.json());
+
+const database = new MongoConnection();
+database.connect();
+
+const urlController = new URLController();
+api.post('/shorten', URLController.shorten);
+api.get('/:hash', urlController.redirect);
+
+api.listen(5000, () => console.log('Express listen'));
+
+
+
+
